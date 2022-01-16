dba72b9d1a25214af2b0d6564a9b9a51b2ecc6d1
Paskuvan <majopaskuvan@gmail.com>
2022-01-16 18:58:51 -0300

    add navbar jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+
+
+
 
 const Home = () => {
     return (
-        <div>
-            Homepage
+        <div className="container">
+            <Navbar/>
         </div>
     )
 }
