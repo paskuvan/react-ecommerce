5ec13b4a845645e8bdd18bbf508c31939dc95552
Paskuvan <majopaskuvan@gmail.com>
2022-01-19 15:17:06 -0300

    responsive first part

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
 import { Email, Facebook, Instagram, Phone, Room, Twitter, YouTube } from '@material-ui/icons';
 import styled from 'styled-components'
+import { mobile } from "../responsive";
 
 const Container = styled.div`
         display:flex;
+        ${mobile({ flexDirection: "column" })}
     `;
 const Left = styled.div`
         flex:1;
@@ -35,6 +37,7 @@ const Left = styled.div`
 const Center = styled.div`
     flex: 1;
     padding:20px;
+    ${mobile({ display: "none" })}
 `;
 
 const Title =styled.h3`
@@ -57,6 +60,7 @@ const ListItem =styled.li`
 const Right = styled.div`
     flex:1;
     padding: 20px;
+    ${mobile({ backgroundColor: "#eee" })}
 `;
 
 const ContactItem = styled.div`
